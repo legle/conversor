2bf7a85ab45aac95c198eb5c5cce88fa6cbc398c
Leonardo Gleison <leonardo.gleison@nubank.com.br>
2022-05-10 18:42:25 -0300

    adding a functiontest  to parce param values

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -1,8 +1,19 @@
 (ns conversor.core
   (:gen-class))
 
+(defn valores-em
+  [argumento]
+  (cond
+    (.startsWith argumento "--de=")
+    {:de (.substring argumento 5)}
+    (.startsWith argumento "--para=")
+    {:para (.substring argumento 7)}
+    :else {}))
+
+
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Temos " (count args) " Argumentos")
-  (println "Os argumentos são: " args))
+  (println (map valores-em args)))
