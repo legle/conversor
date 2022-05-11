3116007d7a1ce03b21428253afbcfddcb3f5dd6c
Leonardo Gleison <leonardo.gleison@nubank.com.br>
2022-05-11 13:14:37 -0300

    adding http functionality

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -1,12 +1,24 @@
 (ns conversor.core
-  (:require [clojure.tools.cli :refer [parse-opts]])
+  (:require [clojure.tools.cli :refer [parse-opts]]
+            [clj-http.client :as http-client])
   (:gen-class))
+
+(def api-url "https://economia.awesomeapi.com.br/json/last")
 
 (def opcoes-do-programa [["-d" "--de Moeda base" "Moeda base para conversão" :default "brl"]
                          ["-p" "--para moeda destino" "Moeda a qual queremos saber o valor" :default "usd"]])
 
+(defn parametrizar-moedas
+  [de para]
+  (str de "-" para))
+
+(defn compor-url
+  [moedas]
+  (str api-url "/" moedas))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
-  (prn "As opções são:"
-       (:options (parse-opts args opcoes-do-programa))))
+  (let [{:keys [de para]} (:options (parse-opts args opcoes-do-programa))]
+    (prn "Cotação atual:"
+         (http-client/get (compor-url (parametrizar-moedas de para))))))
