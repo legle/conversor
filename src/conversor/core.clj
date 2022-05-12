0f9560afb6fbb810cf290344f3d2fd8a98f0d67d
Leonardo Gleison <leonardo.gleison@nubank.com.br>
2022-05-11 21:22:09 -0300

    refactoring to isolate te http query

--- src/conversor/core.clj
+++ src/conversor/core.clj
@@ -17,11 +17,19 @@
   [moedas]
   (str api-url "/" moedas))
 
+(defn consultar-cotacao!
+  [de para]
+  (-> (:body (http-client/get (compor-url (parametrizar-moedas de para))))
+      (parse-string)
+      (get-in ["BRLUSD" "ask"])))
+
+(defn formatar-resultado
+  [cotacao]
+  (prn cotacao)
+  (str "A cotação da moeda é " cotacao))
+
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (let [{:keys [de para]} (:options (parse-opts args opcoes-do-programa))]
-    (-> (:body (http-client/get (compor-url (parametrizar-moedas de para))))
-        (parse-string)
-        (get-in ["USDBRL" "bid"])
-        (prn))))
+    (prn (formatar-resultado (consultar-cotacao! de para)))))
